a813d16c63a7c448040406bd47718e22eec80345
Nik Taormina <c15nikolas.taormina@usafa.edu>
2013-11-19 15:30:20 -0700

    Now with reverse

--- motors.c
+++ motors.c
@@ -8,66 +8,74 @@
 #include "motors.h"
 
 void turnRightSlight() {
-	P1DIR |= BIT6 | BIT7;
-	//P1REN |= BIT6 | BIT7;
+	P1DIR |= BIT4 | BIT5 | BIT6 | BIT7;
+	P1REN |= BIT4 | BIT5 | BIT6 | BIT7;
 	P1OUT |= BIT6;
+	P1OUT &= ~(BIT4 | BIT5 | BIT7);
 	__delay_cycles(1000000);
-	P1OUT &= ~(BIT6 | BIT7);
+	P1OUT &= ~(BIT4 | BIT5 | BIT6 | BIT7);
 
 }
 
 void turnLeftSlight() {
-	P1DIR |= BIT6 | BIT7;
-	//P1REN |= BIT6 | BIT7;
+	P1DIR |= BIT4 | BIT5 | BIT6 | BIT7;
+	P1REN |= BIT4 | BIT5 | BIT6 | BIT7;
 	P1OUT |= BIT7;
+	P1OUT &= ~(BIT4 | BIT5 | BIT6);
 	__delay_cycles(1000000);
-	P1OUT &= ~(BIT6 | BIT7);
+	P1OUT &= ~(BIT4 | BIT5 | BIT6 | BIT7);
 }
 
 void turnRightLarge() {
-	P1DIR |= BIT6 | BIT7;
-	//P1REN |= BIT6 | BIT7;
+	P1DIR |= BIT4 | BIT5 | BIT6 | BIT7;
+	P1REN |= BIT4 | BIT5 | BIT6 | BIT7;
 	P1OUT |= BIT6;
+	P1OUT &= ~(BIT4 | BIT5 | BIT7);
 	__delay_cycles(2000000);
-	P1OUT &= ~(BIT6 | BIT7);
+	P1OUT &= ~(BIT4 | BIT5 | BIT6 | BIT7);
 }
 
 void turnLeftLarge() {
-	P1DIR |= BIT6 | BIT7;
-	//P1REN |= BIT6 | BIT7;
+	P1DIR |= BIT4 | BIT5 | BIT6 | BIT7;
+	P1REN |= BIT4 | BIT5 | BIT6 | BIT7;
 	P1OUT |= BIT7;
+	P1OUT &= ~(BIT4 | BIT5 | BIT6);
 	__delay_cycles(2000000);
+	P1OUT &= ~(BIT4 | BIT5 | BIT6 | BIT7);
+}
+
+void forward() {
+	P1DIR |= BIT4 | BIT5 | BIT6 | BIT7;
+	P1REN |= BIT4 | BIT5 | BIT6 | BIT7;
+	P1OUT |= BIT6 | BIT7;
+	P1OUT &= ~(BIT4 | BIT5);
+}
+void reverse() {
+	P1DIR |= BIT4 | BIT5 | BIT6 | BIT7;
+	P1REN |= BIT4 | BIT5 | BIT6 | BIT7;
+	P1OUT |= BIT4 | BIT5;
 	P1OUT &= ~(BIT6 | BIT7);
 }
 
-void forward(){
-	P1DIR |= BIT6 | BIT7;
-//	P1REN |= BIT6 | BIT7;
-	P1OUT |= BIT6 | BIT7;
+void setUpTimerA() {
+
+	P1DIR |= BIT2;                // TA0CCR1 on P1.2
+	P1SEL |= BIT2;                // TA0CCR1 on P1.2
+
+	P2DIR |= BIT2;                // TA0CCR1 on P2.2
+	P2SEL |= BIT2;                // TA0CCR1 on P2.2
+
+	TACTL &= ~MC1 | MC0;            // stop timer A0
+
+	TACTL |= TACLR;                // clear timer A0
+
+	TACTL |= TASSEL1;           // configure for SMCLK
+
+	TACCR0 = 100;   // set signal period to 100 clock cycles (~100 microseconds)
+
+	TACCR1 = 60;
+	TACCTL1 |= OUTMOD_7;        // set TACCTL1 to Reset / Set mode
+
+	TACTL |= MC0;                // count up
 }
-void reverse();
-
-void setUpTimerA(){
-
-
-			P1DIR |= BIT2;                // TA0CCR1 on P1.2
-	        P1SEL |= BIT2;                // TA0CCR1 on P1.2
-
-	        P2DIR |= BIT2;                // TA0CCR1 on P2.2
-	        P2SEL |= BIT2;                // TA0CCR1 on P2.2
-
-	        TACTL &= ~MC1|MC0;            // stop timer A0
-
-	        TACTL |= TACLR;                // clear timer A0
-
-	        TACTL |= TASSEL1;           // configure for SMCLK
-
-	        TACCR0 = 100;                // set signal period to 100 clock cycles (~100 microseconds)
-
-	        TACCR1 = 60;
-	        TACCTL1 |= OUTMOD_7;        // set TACCTL1 to Reset / Set mode
-
-	        TACTL |= MC0;                // count up
-}
-
 
