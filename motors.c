a1a9320fc49e47151e69dc3c8658ab88bba1102a
Nik Taormina <c15nikolas.taormina@usafa.edu>
2013-11-19 20:02:35 -0700

    Uses all four controller outs

--- motors.c
+++ motors.c
@@ -8,53 +8,58 @@
 #include "motors.h"
 
 void turnRightSlight() {
-	P1DIR |= BIT4 | BIT5 | BIT6 | BIT7;
-	P1REN |= BIT4 | BIT5 | BIT6 | BIT7;
-	P1OUT |= BIT6;
-	P1OUT &= ~(BIT4 | BIT5 | BIT7);
+	P1DIR |= BIT7 | BIT5 | BIT6;
+	P1REN |= BIT7 | BIT5 | BIT6;
+	P1OUT |= BIT7 | BIT5;
+	P1OUT &= ~(BIT1 | BIT3 | BIT4);
 	__delay_cycles(1000000);
-	P1OUT &= ~(BIT4 | BIT5 | BIT6 | BIT7);
+	P1OUT &= ~(BIT1 | BIT3 | BIT4 | BIT5 | BIT6 | BIT7);
 
 }
 
 void turnLeftSlight() {
-	P1DIR |= BIT4 | BIT5 | BIT6 | BIT7;
-	P1REN |= BIT4 | BIT5 | BIT6 | BIT7;
-	P1OUT |= BIT7;
-	P1OUT &= ~(BIT4 | BIT5 | BIT6);
+	P1DIR |= BIT1 | BIT3 | BIT4;
+	P1REN |= BIT1 | BIT3 | BIT4;
+	P1OUT |= BIT1 | BIT3;
+	P1OUT &= ~(BIT5 | BIT6 | BIT7);
 	__delay_cycles(1000000);
-	P1OUT &= ~(BIT4 | BIT5 | BIT6 | BIT7);
+	P1OUT &= ~(BIT1 | BIT3 | BIT4 | BIT5 | BIT6 | BIT7);
+
 }
 
 void turnRightLarge() {
-	P1DIR |= BIT4 | BIT5 | BIT6 | BIT7;
-	P1REN |= BIT4 | BIT5 | BIT6 | BIT7;
-	P1OUT |= BIT6;
-	P1OUT &= ~(BIT4 | BIT5 | BIT7);
+	P1DIR |= BIT7 | BIT5 | BIT6;
+	P1REN |= BIT7 | BIT5 | BIT6;
+	P1OUT |= BIT7 | BIT5;
+	P1OUT &= ~(BIT1 | BIT3 | BIT4);
 	__delay_cycles(2000000);
-	P1OUT &= ~(BIT4 | BIT5 | BIT6 | BIT7);
+	P1OUT &= ~(BIT1 | BIT3 | BIT4 | BIT5 | BIT6 | BIT7);
+
 }
 
 void turnLeftLarge() {
-	P1DIR |= BIT4 | BIT5 | BIT6 | BIT7;
-	P1REN |= BIT4 | BIT5 | BIT6 | BIT7;
-	P1OUT |= BIT7;
-	P1OUT &= ~(BIT4 | BIT5 | BIT6);
+	P1DIR |= BIT1 | BIT3 | BIT4;
+	P1REN |= BIT1 | BIT3 | BIT4;
+	P1OUT |= BIT1 | BIT3;
+	P1OUT &= ~(BIT5 | BIT6 | BIT7);
 	__delay_cycles(2000000);
-	P1OUT &= ~(BIT4 | BIT5 | BIT6 | BIT7);
+	P1OUT &= ~(BIT1 | BIT3 | BIT4 | BIT5 | BIT6 | BIT7);
+
 }
 
 void forward() {
-	P1DIR |= BIT4 | BIT5 | BIT6 | BIT7;
-	P1REN |= BIT4 | BIT5 | BIT6 | BIT7;
-	P1OUT |= BIT6 | BIT7;
-	P1OUT &= ~(BIT4 | BIT5);
+	P1DIR |= BIT1 | BIT3 | BIT4 | BIT7 | BIT5 | BIT6;
+	P1REN |= BIT1 | BIT3 | BIT4 | BIT7 | BIT5 | BIT6;
+	P1OUT |= BIT1 | BIT3 | BIT7 | BIT5;
+	P1OUT &= ~(BIT4 | BIT6);
+
 }
 void reverse() {
-	P1DIR |= BIT4 | BIT5 | BIT6 | BIT7;
-	P1REN |= BIT4 | BIT5 | BIT6 | BIT7;
-	P1OUT |= BIT4 | BIT5;
-	P1OUT &= ~(BIT6 | BIT7);
+	P1DIR |= BIT1 | BIT3 | BIT4 | BIT7 | BIT5 | BIT6;
+	P1REN |= BIT1 | BIT3 | BIT4 | BIT7 | BIT5 | BIT6;
+	P1OUT &= ~(BIT1 | BIT3 | BIT7 | BIT5);
+	P1OUT |= BIT4 | BIT6;
+
 }
 
 void setUpTimerA() {
@@ -62,8 +67,8 @@ void setUpTimerA() {
 	P1DIR |= BIT2;                // TA0CCR1 on P1.2
 	P1SEL |= BIT2;                // TA0CCR1 on P1.2
 
-	P2DIR |= BIT2;                // TA0CCR1 on P2.2
-	P2SEL |= BIT2;                // TA0CCR1 on P2.2
+//	P2DIR |= BIT2;                // TA0CCR1 on P2.2
+//	P2SEL |= BIT2;                // TA0CCR1 on P2.2
 
 	TACTL &= ~MC1 | MC0;            // stop timer A0
 
